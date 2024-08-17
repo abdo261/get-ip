76ef0816cecceb9b873b496ae2caa60f69aacc05
Obsku <abdellah.basketeur2015@gmail.com>
2024-08-17 21:44:50 +0100

    mmmmm

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const cors = require('cors');  // Correct import for cors package
 const app = express();
 const port = 3000;
-const core = require('cors')
 
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000']
+}));
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+// Middleware to log IP
 app.use((req, res, next) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
   console.log('Client IP:', ip);
   next();
 });
-app.use(core({
-    origin:['http://localhost:5173',"http://localhost:3000"]
-}))
 
+// Endpoint to get IP address
 app.get('/api/get-ip', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-
   
+  // Format IP Address
   let formattedIP = ip;
   if (ip.includes('::ffff:')) {
     formattedIP = ip.split('::ffff:')[1];
@@ -24,6 +29,13 @@ app.get('/api/get-ip', (req, res) => {
   }
 
   res.json({ ip: formattedIP });
+});
+
+// Endpoint to receive IP from client
+app.post('/api/save-ip', (req, res) => {
+  const { ip } = req.body;
+  console.log('Received local IP:', ip);
+  res.json({ message: 'IP received', ip });
 });
 
 app.listen(port, () => {
